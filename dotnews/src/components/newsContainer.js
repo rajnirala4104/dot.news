e1f8686dd9cb259f823d934e3d3a4b63567b10bd
Raj Nirala <rajnirala4104@gmail.com>
2023-05-15 11:51:03 +0530

    fetch data from api

--- dotnews/src/components/newsContainer.js
+++ dotnews/src/components/newsContainer.js
@@ -4,8 +4,23 @@ import {useState} from 'react'
 function NewsContainer(props) {
 
     const [newsState, setNewsState] = useState([])
-    const API = ""
-
+    const API = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2139974812e64d308b801a24cb53aa65"
+    // const newsDicArr = [];
+    fetch(API)
+    .then(response => response.json())
+    .then(data => {
+        data.articles.forEach(newsDic => {
+            newsState.push({
+                newsPublishTime: newsState.publishedAt,
+                newsTitle: newsDic.title,
+                newsDesctiption : newsDic.desctiption,
+                newsImageUrl: newsDic.urlToImage,
+                newsReadMoreUrl: newsDic.url
+            })
+        });
+    }, err=>console.log(err))
+    console.log(newsState)
+    setNewsState(newsState)
     return (
         <div className="container">
             <NewsBox />
