e7a723c4e59b7488baf436442094a4f2d078db7a
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-07-14 11:49:19 +0530

    litbit changes

---
+++ dotnews/src/api/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2139974812e64d308b801a24cb53aa65";
--- dotnews/src/components/newsContainer.js
+++ dotnews/src/components/newsContainer.js
@@ -1,36 +1,26 @@
+import { useState } from "react";
+import "./css/newsContainer.css";
 import NewsBox from "./newsBox";
-import { useState } from 'react'
-import './css/newsContainer.css'
 
 function NewsContainer(props) {
+  const [newsState, setNewsState] = useState([]);
 
-    const [newsState, setNewsState] = useState([])
-    const API = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2139974812e64d308b801a24cb53aa65"
-    // const newsDicArr = [];
-    fetch(API)
-        .then(response => response.json())
-        .then(data => {
-            data.articles.forEach(newsDic => {
-                newsState.push({
-                    newsPublishTime: newsDic.publishedAt,
-                    newsTitle: newsDic.title,
-                    newsDescription: newsDic.description,
-                    newsImageUrl: newsDic.urlToImage,
-                    newsReadMoreUrl: newsDic.url
-                })
-                console.log(`-title : ${newsDic.title}\n-description: ${newsDic.description}\n-imageUrl: ${newsDic.urlToImage}\n-Time: ${newsDic.publishedAt}`)
-            });
-        }, err => console.log(err))
-
-    return (
-        <div className="container newsContainer row">
-            {newsState.map((news, index) => {
-                return (
-                    <NewsBox key={index} newsPublishTime={news.newsPublishTime} newsTitle={news.newsTitle} newsDescription={news.newsDescription} newsImageUrl={news.newsImageUrl}/>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="container newsContainer row">
+      {newsState.map((news, index) => {
+        console.log(news);
+        return (
+          <NewsBox
+            key={index}
+            newsPublishTime={news.newsPublishTime}
+            newsTitle={news.newsTitle}
+            newsDescription={news.newsDescription}
+            newsImageUrl={news.newsImageUrl}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-export default NewsContainer
+export default NewsContainer;
